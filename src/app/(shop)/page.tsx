b09285f585eba15d77a080eb91bf6cb9aa433aea
Huguez <carlos.huguez@gmail.com>
2024-04-02 16:57:07 -0700

    title component and style in shoplayout

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,11 @@
 
-import { TopMenu } from "@/components";
-import { monse } from "@/config/fonts";
+import { Title } from "@/components";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className={ monse.className +" font-bold" }> Hello world!!! </h1>
-    </main>
+    <>
+      <Title title="Men's" subtitle="All products" />  
+    </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 
 export { TopMenu } from "./ui/topMenu/TopMenu"
 export { PageNotFound } from "./ui/not-found/PageNotFound"
+export { Title } from "./ui/title/Title"
