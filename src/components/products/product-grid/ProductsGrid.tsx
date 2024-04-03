bf4172ceda5749d77d2e0fa19346befc19292be3
Huguez <carlos.huguez@gmail.com>
2024-04-02 23:03:06 -0700

    ProductGridItem first steps

--- src/components/products/product-grid/ProductsGrid.tsx
+++ src/components/products/product-grid/ProductsGrid.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/interfaces";
+import { ProductGridItem } from "@/components";
 
 interface propsI {
    products: Product[];
@@ -7,8 +8,9 @@ interface propsI {
 export const ProductsGrid = ({ products }: propsI ) => {
    return (
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10 mb-10 ">
-         { products.map( ( item, index ) => (
-            <span key={`${ item.title }-${item.title}-${ index }`}> { item.title } </span>
+         { products.filter( product => product.gender === "men" || product.gender === "unisex" )
+            .map( ( item, index ) => (
+            <ProductGridItem key={`${ item.slug }-${ index }`}  { ...item } />
          ) ) }
       </div>
    )
---
+++ src/components/products/productItem/ProductGridItem.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { Product } from '@/interfaces'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+
+export const ProductGridItem = ( { description, images, inStock, price, sizes, slug, tags, title, type, gender }:Product ) => {
+   
+   const [ displayImage, setDisplayImage ] = useState<string>( images[0] )
+
+   return (
+      <div className='overflow-hidden fade-in hover:text-blue-500 shadow-md rounded-lg' >
+
+         <Link href={`/product/${ slug }`}>
+            <Image
+               src={ "/products/"+displayImage }
+               alt={ description }
+               className='w-full object-cover '
+               width={500}
+               height={500}
+               onMouseOver={ () => setDisplayImage( images[1] ) } 
+               onMouseLeave={ () => setDisplayImage( images[0] ) }
+            />
+         </Link>
+
+         <div className="flex flex-col p-4 ">
+            <Link href={`/product/${ slug }`} className=''>
+               { title }
+            </Link>
+            <span className='font-bold text-black'>${ price }</span>
+         </div>
+         
+      </div>
+   )
+}
